f79829d175dec192e7e3a854a9678e2a912c73f6
layout-max <70515554+layout-max@users.noreply.github.com>
2020-09-26 14:57:20 +0900

    userテーブルの作成

--- db/migrate/20200926031050_devise_create_users.rb
+++ db/migrate/20200926031050_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :zenkaku_familyname,   null: false
+      t.string :zenkaku_name,         null: false
+      t.string :katakana_family_kana, null: false
+      t.string :katakana_name_kana,   null: false
+      t.string :email,                null: false, default: ""
+      t.string :encrypted_password,   null: false, default: ""
+      t.string :nickname,             null: false
+      t.date :birthday,               null: false
 
       ## Recoverable
       t.string   :reset_password_token

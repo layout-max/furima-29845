d7516f20eef728ebf5b6f7cae5e59676532dcbb8
layout-max <70515554+layout-max@users.noreply.github.com>
2020-09-26 22:03:55 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:zenkaku_familyname, :zenkaku_name, :katakana_family_kana, :katakana_name_kana, :email, :nickname, :birthday]
+      )
   end
 
   def basic_auth

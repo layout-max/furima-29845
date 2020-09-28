21685627ef1218c6d138389df7c9c40123c88c3b
layout-max <70515554+layout-max@users.noreply.github.com>
2020-09-28 18:23:49 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
+
+  private
+
+  def items_params
+    params.require(:items).permit(:text, :image).merge(user_id: current_user.id)
+  end
   
   def index
   end

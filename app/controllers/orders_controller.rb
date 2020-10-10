9bef707332ed5408a5b5eb798e461ed57428b807
layout-max <70515554+layout-max@users.noreply.github.com>
2020-10-10 10:27:20 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index, only: [:index]
   before_action :SetItem, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
     if @item.user_id == current_user.id
